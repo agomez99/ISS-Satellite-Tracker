ede1b9ae1373942ba0ad0b4a81c4f6fb69ff7022
Travis Guillory <travisguillory70@gmail.com>
2020-03-11 19:31:17 -0500

    final commit

--- js/main.js
+++ js/main.js
@@ -1,195 +1,90 @@
 // Main JS COde here.
-var width = 960,
-  height = 480,
-  zoom = 1;
+$(document).ready(function() {
+  // map is reponsive. requires a page load will not re-size with a window size change
+  var width = $("#map").width() * 0.85,
+    height = width / 2;
 
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+   var svg = d3
+    .select("#map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-var projection = d3.geo
-  .equirectangular()
-  .scale(153)
-  .translate([width / 2, height / 2]);
+  var projection = d3.geo
+    .equirectangular()
+    .scale(width / 1.89 / Math.PI)
+    .translate([width / 2, height / 2]);
 
-var path = d3.geo.path().projection(projection);
+  var path = d3.geo.path().projection(projection);
 
-var graticule = d3.geo.graticule();
+  var graticule = d3.geo.graticule();
 
-var lat;
-var lon;
-let x;
-let latRad;
-<<<<<<< HEAD
-let mercN;
-=======
+  var imgTransformFactor = -(width * 0.1) / 2;
 
->>>>>>> agfeature
-let y;
+  d3.json("js/world-110m.json", function(error, world) {
+    svg
+      .style("background-color", "#1d87c5")
+      .style("border", "1px solid black")
+      .append("g")
+      .attr("class", "land")
+      .selectAll("path")
+      .data([topojson.object(world, world.objects.land)])
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("fill", "#1c8f4c");
+    svg
+      .append("g")
+      .attr("class", "boundary")
+      .selectAll("boundary")
+      .data([topojson.object(world, world.objects.countries)])
+      .enter()
+      .append("path")
+      .attr("fill", "#1c8f4c")
+      .attr("stroke", "#766951")
+      .attr("d", path);
+    svg
+      .append("g")
+      .attr("class", "graticule")
+      .selectAll("path")
+      .data(graticule.lines)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("stroke", "grey")
+      .attr("opacity", "0.5");
 
-var userLon;
+    getCurrentStationPosition();
+  });
 
-d3.json("js/world-110m.json", function(error, world) {
-  svg
-    .append("g")
-    .attr("class", "land")
-    .selectAll("path")
-    .data([topojson.object(world, world.objects.land)])
-    .enter()
-    .append("path")
-    .attr("d", path);
-  svg
-    .append("g")
-    .attr("class", "boundary")
-    .selectAll("boundary")
-    .data([topojson.object(world, world.objects.countries)])
-    .enter()
-    .append("path")
-    .attr("d", path);
-  svg
-    .append("g")
-    .attr("class", "graticule")
-    .selectAll("path")
-    .data(graticule.lines)
-    .enter()
-    .append("path")
-    .attr("d", path);
+  
+ 
 
-  getFutureStationPositions();
-  getCurrentStationPosition();
-});
-
-function drawCurrentLocation(coord) {
-  svg
-<<<<<<< HEAD
-    .append("circle")
-    .attr("cx", coord[0])
-    .attr("cy", coord[1])
-    .attr("r", 10)
-    .style("z-index", 1)
-    .attr("fill", "yellow");
-=======
-    .append("image")
-    .attr("x", coord[0])
-    .attr("y", coord[1])
-    .attr("href", "img/station.png")
-    .attr("height", 80)
-    .attr("width", 80)
-    // .attr("r", 10)
-    // .style("z-index", 1)
-    // // .attr("fill", "yellow");
->>>>>>> agfeature
-}
-
-function drawFuturePath(coordArray) {
-  // loop to get x, y of lat and long coordinates from the api
-
-  svg
-    .selectAll("circle")
-    .data(coordArray)
-    .enter()
-    .append("circle")
-    .attr("cx", (d, i) => {
-      return d[0];
-    })
-    .attr("cy", (d, i) => {
-      return d[1];
-    })
-    .attr("r", 5)
-    .style("z-index", 2)
-    .attr("fill", "red")
-    .text("z");
-}
-
-function getCurrentStationPosition() {
-  console.log("Getting Station");
-  fetch(
-    "https://www.n2yo.com/rest/v1/satellite/positions/25544/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII"
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      
-
-      drawCurrentLocation(
-        projection([
-          data.positions[0].satlatitude,
-          data.positions[0].satlongitude
-        ])
-      );
-
-<<<<<<< HEAD
-      //console.log(mercator(data.positions[0].satlatitude, data.positions[0].satlongitude ))
-=======
-      
->>>>>>> agfeature
-    });
-}
-
-function getFutureStationPositions() {
-  fetch(
-    // "https://www.n2yo.com/rest/v1/satellite/positions/25544/29.5891833/-98.6270735/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII/"
-    "https://api.wheretheiss.at/v1/satellites/25544/tles"
+  function getCurrentStationPosition() {
+    console.log("Getting Station");
+    fetch(
+      "https://www.n2yo.com/rest/v1/satellite/positions/25544/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII"
     )
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-
-<<<<<<< HEAD
-      let arr = [];
-      /* data.forEach((element) => {
-              
-               //console.log(element.satlatitude);
-               
-               arr.push(mercator(element.satlatitude, element.satlongitude)
-               );
-              
-              drawFuturePath(arr);
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let coord = projection([
+          data.positions[0].satlongitude,
+          data.positions[0].satlatitude
+        ]);
         
-        }); */
-      //console.log(arr)
-     
-      let iss = new Satellite(data.tle_timestamp, data.id, data.name, data.header, data.tle1, data.tle2);
-
-
-    
-    });
-
-}
-
-class Satellite {
-    constructor(timeStamp, satId, satName,  header, tle1, tle2 ){
-        this.timeStamp = timeStamp;
-        this.satId = satId;
-        this.satName = satName;
-        this.satHeader = header;
-        this.tle1 = tle1;
-        this.tle2 = tle2;
-    }
-    
-getId = ()=>{
-    if(this.satId){
-        return this.satId;
-    }else {
-        return "No ID Found";
-    }
-
-getTimeStamp = ()
-}
-=======
-      
-     
-    
-
-
-    
-    });
->>>>>>> agfeature
-
-}
-
+        svg
+          .append("image")
+          .attr("x", coord[0])
+          .attr("y", coord[1])
+          .attr("href", "img/station.png")
+          .attr("height", width * 0.1)
+          .attr("width", width * 0.1)
+          .attr(
+            "transform",
+            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+          );
+      });
+  }
+});
--- script.js
+++ script.js
@@ -1,81 +1,51 @@
-$(document).ready(function(){
-
-//LAST FIVE DAYS APOD
-var space = "https://api.nasa.gov/planetary/apod?api_key=CScJj85NGTPLG7hllUbm393T3rjnQO8ke11aHB2m&start_date=2020-03-03&end_date=2020-03-08"
-
-//TODAY APOD
-var space2 = "https://api.nasa.gov/planetary/apod?api_key=CScJj85NGTPLG7hllUbm393T3rjnQO8ke11aHB2m"
-//ISS Future positions
-var cords = "https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII"
-
-$.ajax({
+$(document).ready(function() {
+  //TODAY APOD
+  var space2 =
+    "https://api.nasa.gov/planetary/apod?api_key=CScJj85NGTPLG7hllUbm393T3rjnQO8ke11aHB2m";
+  //ISS Future positions
+  var cords =
+    "https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
+  $.ajax({
     url: space2,
     method: "GET"
   }).then(function(response) {
     $("#spaceTitle").html(response.title);
     $("#Sdate").html(response.date);
     $("#info").html(response.explanation);
-    $("#spaceImg").attr("src",response.url);
+    $("#spaceImg").attr("src", response.url);
   });
-
   $.ajax({
     url: cords,
     method: "GET"
   }).then(function(response) {
-
-	$("#satName").html("Satelite Name: " + response.info.satname);
-
-
-//LAST FIVE DAYS APOD
-var space = "https://api.nasa.gov/planetary/apod?api_key=CScJj85NGTPLG7hllUbm393T3rjnQO8ke11aHB2m&"
-
-var cords = "https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII"
-
-
-
-$.ajax({
-    url: space,
-    method: "GET"
-  }).then(function(response) {
-    // create a element
-    console.log(response[0]);
-    // add this element to the body
-    $("#spaceTitle").html(response[0].title);
-    $("#Sdate").html(response[0].date);
-     $("#info").html(response[0].explanation);
-    $("#spaceImg").attr("src",response[0].url);
-
-    
-  });
-
-$("#satName").html("Satelite Name: " + response.info.satname);
-
-
+    $("#satName").html("Satelite Name: " + response.info.satname);
     for (i in response.positions) {
-    y = response.positions[i].satlatitude;
-    x = response.positions[i].satlongitude;
-//$("#stat1").html("latitude: " + y);
-//$("#stat2").html("longitude: "+x);
-
-
-}
-var times = response.positions[0].timestamp;
-var ts = times;
-var ts_ms = ts * 1000;
-var date_ob = new Date(ts_ms);
-var year = date_ob.getFullYear();
-var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-var date = ("0" + date_ob.getDate()).slice(-2);
-var hours = ("0" + date_ob.getHours()).slice(-2);
-var minutes = ("0" + date_ob.getMinutes()).slice(-2);
-var seconds = ("0" + date_ob.getSeconds()).slice(-2);
-
-$("#timeStamp").html("time stamp "  + year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+      y = response.positions[i].satlatitude;
+      x = response.positions[i].satlongitude;
+    }
+    var times = response.positions[0].timestamp;
+    var ts = times;
+    var ts_ms = ts * 1000;
+    var date_ob = new Date(ts_ms);
+    var year = date_ob.getFullYear();
+    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    var date = ("0" + date_ob.getDate()).slice(-2);
+    var hours = ("0" + date_ob.getHours()).slice(-2);
+    var minutes = ("0" + date_ob.getMinutes()).slice(-2);
+    var seconds = ("0" + date_ob.getSeconds()).slice(-2);
+    $("#timeStamp").html(
+      "time stamp " +
+        year +
+        "-" +
+        month +
+        "-" +
+        date +
+        " " +
+        hours +
+        ":" +
+        minutes +
+        ":" +
+        seconds
+    );
   });
-
-})
-
-   
-
-
-
+});
